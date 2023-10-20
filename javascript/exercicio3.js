ad410564d3aeb31e7a671b9b2f159619edf5436a
danilo <danilosena.ti>
2023-10-20 02:04:59 -0300

    first commit

---
+++ javascript/exercicio3.js
@@ -0,0 +1,7 @@
+/** Utilize if...else para escrever um código que defina três variáveis com os valores dos três ângulos internos de um triângulo.
+Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. 
+Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
+
+Dica: para os ângulos serem de um triângulo válido, a soma dos três ângulos deve ser 180 graus.
+Um ângulo será considerado inválido se não tiver um valor positivo. */
+
---
+++ javascript/operadorNOT.js
@@ -0,0 +1,9 @@
+console.log((2 + 2) === 4); // POSSUEM OS MESMOS VALORES E TIPO.
+
+console.log(!(2 + 2) === 4); // A NEGAÇAO DO RESULTADO INFORMADO A CIMA.
+
+/**
+ * Estamos diante de um operador muito poderoso. Ele pode inverter o valor booleano de
+um elemento. Isso mesmo! Se tivermos uma variável ou um valor considerado true,
+podemos gerar o resultado oposto simplesmente fazendo '!'variável, ou seja, false.
+ */
