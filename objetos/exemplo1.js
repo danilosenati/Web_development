fa66936ad44eed69c51d17274366500d6a70b22f
danilo <danilosena.ti>
2023-11-02 17:38:26 -0300

    objetos

--- objetos/exemplo1.js
+++ objetos/exemplo1.js
@@ -16,12 +16,13 @@ let pessoa = {
 
 let informacoes = {
     descricao: '',
-    notas: []
+    preco: []
+
 }
 
 const motos = {
     marca: ['Yamaha', 'Suzuki', 'Honda'],
-    modelo: ['Factor','V-STROM', 'Titan'],
+    modelo: ['Factor', 'V-STROM', 'Titan'],
     ano: [2022, 2023, 2021]
 }
 
@@ -30,4 +31,30 @@ console.log(motos.marca);
 console.log(`Nome do propritário: ${pessoa.nome}, marca da moto: ${motos.marca[2]}`);
 
 // UTLIZANDO O "['']" PARA ACESSAR UMA PROPRIEDADE:
+console.log(motos['modelo']);
+console.log(`Marcas da moto: ${motos['marca']}`);
 
+// REALIZANDO OPERAÇÃO MATEMATICA CALCULO DO IMC:
+// toFixed(x) limita o número de casas decimais.
+
+let imc = pessoa.peso / (pessoa.altura * pessoa.altura);
+console.log(`Peso: ${imc.toFixed(2)}`);
+
+console.log('----------------------------------');
+
+// ALTERANDO OU ATUALIZANDO O VALOR DA PROPRIEDADE:
+pessoa.nome = 'Rafael Sampaio';
+informacoes.descricao = 'Todos os direitos reservados';
+informacoes.preco = ['20.200'];
+
+console.log(`Nome atualizado: ${pessoa.nome}\nInformações: ${informacoes.descricao}\nNota: ${informacoes.notas}`);
+
+console.log('------------------------------------------------');
+
+// UTILIZANDO SPREED OPERATOR PARA ATUALIZAR OU ALTERAR OS VALORES DA PROPRIEDADE:
+informacoes.preco = [...informacoes.preco, 900.00, 7.800, 16.500]; // ...informacoes.preco evita que o primeiro valor seja subscrito por 900.00.
+motos.ano = [...motos.ano, 1950,20021, 2025];
+
+console.log(`Idades: ${pessoa['idade']}`);
+console.log(`Preços: ${informacoes.preco}`);
+console.log(`${motos['ano']}`);
