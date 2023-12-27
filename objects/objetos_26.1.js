49392e6eed0e8d5ab54b8366841e1f12efbe7602
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2023-12-27 00:21:57 -0300

    objetos_26.1.js
    Continuação do assunto sobre objetos

---
+++ objects/objetos_26.1.js
@@ -0,0 +1,19 @@
+// Adicionando novas propriedades:
+
+const pessoa = {
+    primeiroNome: 'Nome',
+    ultimoNome: 'Sobrenome',
+    age: 33,
+    dados: {
+        salario: '2.500',
+        profissao: 'Desenvolvedor'
+    }
+}
+
+pessoa.primeiroNome = 'Rafael'
+pessoa.ultimoNome = 'Andrade';
+pessoa.dados.gênero = 'Masculino'
+
+console.log(pessoa);
+
+// ADICIONANDO PROPRIEDADES ATRAVÉS DE FUNÇÃO:
