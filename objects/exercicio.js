735f36a58cbb89b735a9c52b726e0e2212455364
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2023-12-29 01:42:19 -0300

    Create exercicio.js

---
+++ objects/exercicio.js
@@ -0,0 +1,27 @@
+const menu ={
+    guaraná: {
+        marca: 'Dolly',
+        quantidade: 2,
+        preço: 9.50
+    },
+    pizza: {
+        sabor: 'Da casa',
+        tamanho: 'Familia',
+        preço: 25.00
+    },
+    delivery: {
+        entregador: 'Relânpago Marquinhos',
+    }
+}
+
+const customerInfo = (fullMenu) => {
+    // Adicione abaixo as informações necessarias
+}
+
+console.log(customerInfo(menu));
+
+const orderModifier = (fullMenu) => {
+    // Adicione abaixo as informações necessarias
+}
+
+console.log(customerInfo(menu));
