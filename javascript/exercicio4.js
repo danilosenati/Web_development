2600a6ce65b1f51236ea76716080914544ffae13
danilosenati <danilosena.ti@gmail.com>
2024-04-23 14:55:28 -0300

    deleting files

--- javascript/exercicio4.js
+++
@@ -1,6 +0,0 @@
-/**
- * Utilize if...else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for par. 
-    Caso contrário, o código deve retornar false.
-    Faça esse exercício utilizando somente um if. */
-
-    
--- javascript/script.js
+++
@@ -1,5 +0,0 @@
-let numero1 = 3 == '1' /* Compara o valor */
-console.log(numero1)
-
-let numero2 = '3' === '3' /* Compara valor e tipo */
-console.log(numero2)
