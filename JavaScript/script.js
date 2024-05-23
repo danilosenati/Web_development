907fd95e8489ce0c94e2128eccdd68276534f21e
danilosenati <danilosena.ti@gmail.com>
2024-05-23 20:04:05 -0300

    23-05 update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,5 +2,20 @@
 // window.confirm("Deseja realmente sair?")
 // window.prompt("Qual é o seu nome?", "Digite seu nome aqui") //  EXIBE UMA MENSAGEM PARA O USUARIO PRENCHER O NOME
 
-var nome = prompt("Qual é o seu nome?", "Digite seu nome aqui") // variavel nome irá receber o valor digitado pelo usuário
-alert("Seu nome é "+nome) //exibindo o valor digitado pelo usuario.
+// var nome = prompt("Qual é o seu nome?", "Digite seu nome aqui") // variavel nome irá receber o valor digitado pelo usuário
+// alert("Seu nome é "+nome) //exibindo o valor digitado pelo usuario.
+
+// var nome = prompt("Qual é o seu nome?", "Digite seu nome aqui")
+// var sobrenome = prompt("Qual é o seu sobrenome?", "Digite seu nome aqui")
+
+// var titulo = document.getElementById('titulo')
+
+// var nomeCompleto = nome + " "+sobrenome
+// titulo.innerHTML = nomeCompleto
+
+//TRABALHANDO COM LET, VAR, CONST
+var variavel = 13; 
+let variavel2 = "Nome";
+const varvar = 101;
+
+// var pode ser utilizado em qualquer parte do código
