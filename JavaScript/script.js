b05182d55d0b7e131a70b476a2b1050c5d72c3cd
danilosenati <danilosena.ti@gmail.com>
2024-05-23 19:05:22 -0300

    23-05

---
+++ JavaScript/script.js
@@ -0,0 +1,6 @@
+// window.alert("Este é um alerta do Javascript")
+// window.confirm("Deseja realmente sair?")
+// window.prompt("Qual é o seu nome?", "Digite seu nome aqui") //  EXIBE UMA MENSAGEM PARA O USUARIO PRENCHER O NOME
+
+var nome = prompt("Qual é o seu nome?", "Digite seu nome aqui") // variavel nome irá receber o valor digitado pelo usuário
+alert("Seu nome é "+nome) //exibindo o valor digitado pelo usuario.
