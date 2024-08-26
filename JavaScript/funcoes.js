e017ea647e29156f361ad1a1664454c3158dd9a5
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2024-08-26 00:01:25 -0300

    Update funcoes.js

--- JavaScript/funcoes.js
+++ JavaScript/funcoes.js
@@ -1,1 +1,12 @@
+function dirigir{
+  var obstaculos = false;
+  var velocidade = 0;
+  var anguloDirecao = 0;
+  var nomeMotorista = "Jó";
 
+  if((velocidade > 100) || obstaculos == true){
+    alert("Desacelerar");
+  }else{
+    alert("Só "+nomeMotorista+" pode guiar esse veiculo!");
+  }
+}
