fee4e81440f899b21ff137f7f945e7b1423b3cbc
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2024-08-26 00:46:53 -0300

    Create somarDoisNumeros.js
    CODIGO MUDULAR - funções.

---
+++ JavaScript/somarDoisNumeros.js
@@ -0,0 +1,9 @@
+somarDoisNumeros(7, 5);
+somarDoisNumeros(2, -3);
+somarDoisNumeros(9, 4);
+
+function somarDoisNumeros(valor1, valor2){
+
+var soma = (valor1 + valor2);
+alert("Soma dos números : "+ soma);
+}
